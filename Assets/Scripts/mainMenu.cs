38fb4d43b112997cb9c3c9a6041b5c4462df14d4
saginbajracharya <saginbajracharya09@gmail.com>
2019-10-26 18:24:55 +0545

    [Added]:: Scene Game.unity           set up layout and third person camera in game           menu setup PLAY & QUIT

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayButtonClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void QuitButtonClicked()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
