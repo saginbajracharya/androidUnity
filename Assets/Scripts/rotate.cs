3a6cd9d35093dfc4fb6896bbe290610bef4eef73
saginbajracharya <saginbajracharya09@gmail.com>
2019-11-03 14:00:01 +0545

    [added]::unkn

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+	public Transform cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.touchCount == 1)
+		{
+			// GET TOUCH 0
+			Touch touch0 = Input.GetTouch(0);
+
+			// APPLY ROTATION
+			if (touch0.phase == TouchPhase.Moved)
+			{
+				cube.transform.Rotate(0f, touch0.deltaPosition.x, 0f);
+			}
+
+		}
+	}
+}
---
+++ Assets/Scripts/shoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+	public GameObject spawnPoint;
+	public GameObject ExplosionPrefab;
+	public Rigidbody projectilePrefab;
+	public float speed;
+	Rigidbody clone;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		for(var i =0; i < Input.touchCount; ++i)
+		{
+			if(Input.GetTouch(i).phase == TouchPhase.Began )
+			{
+				clone = Instantiate(projectilePrefab, transform.position, transform.rotation) as Rigidbody;
+				clone.velocity = transform.TransformDirection(Vector3.forward * speed);
+				//            Physics.IgnoreCollision ( projectilePrefab.collider, transform.root.collider );
+
+			}
+		}
+
+	}
+}
